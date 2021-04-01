07b50c06d6d2726f4d9ba20ad3f52b18156bca63
Han Myeong-ho <thks1018@gmail.com>
2021-04-01 13:59:55 +0900

    Prepare to merge

--- schema/sh_hooks.lua
+++ schema/sh_hooks.lua
@@ -1,9 +1,20 @@
 
 function Schema:CanPlayerUseBusiness(client, uniqueID)
-	if (client:IsAdmin()) then
-		return true
-	else
-		return false
+	if (client:Team() == FACTION_CITIZEN) then
+		local itemTable = ix.item.list[uniqueID]
+
+		if (itemTable) then
+			if (itemTable.permit) then
+				local character = client:GetCharacter()
+				local inventory = character:GetInventory()
+
+				if (!inventory:HasItem("permit_"..itemTable.permit)) then
+					return false
+				end
+			elseif (itemTable.base ~= "base_permit") then
+				return false
+			end
+		end
 	end
 end
 
@@ -39,6 +50,6 @@ function Schema:CanPlayerEditObjectives(client)
 	return bCanEdit
 end
 
-function Schema:CanDrive(client)
-	return client:IsAdmin()
+function Schema:CanDrive()
+	return false
 end
