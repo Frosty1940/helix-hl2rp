f1f6d735c110bf6a2626de5ef7c1b33430d9d577
Kaiser-Frosty <thks1018@gmail.com>
2021-05-29 12:32:54 +0900

    Revert "NOT GONNA HAPPEN"
    This reverts commit cdbc02328cd129fa743392911176253b3800f686.

--- plugins/vortigaunt_stuff/factions/sh_vortigaunt_slave.lua
+++ plugins/vortigaunt_stuff/factions/sh_vortigaunt_slave.lua
@@ -4,7 +4,7 @@ FACTION.description = "vortigauntDesc"
 FACTION.color = Color(77, 158, 154, 255)
 FACTION.models = {"models/vortigaunt_slave.mdl"}
 FACTION.weapons = {"swep_vortigaunt_sweep"}
-FACTION.isDefault = false
+FACTION.isDefault = true
 FACTION.isGloballyRecognized = false
 
 function FACTION:OnTransfered(client)
