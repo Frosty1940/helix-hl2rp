e9b62c013878b72d4a39437e335c198bd7d0aca4
Frosty <thks1018@gmail.com>
2023-09-04 16:04:42 +0900

    I Fixit

--- plugins/wakeupmessages.lua
+++ plugins/wakeupmessages.lua
@@ -16,17 +16,15 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 -- Feel free to change any of these messages.
 local wakeupMessages = {
-	"You wake up from a long nap and recover from your sleep.",
-	"You stand up and sniffing the fresh air from the world.",
-	"You start to stand up and recover from your sleep.",
-	"You are sweating from all the scare you have been getting from your dream and you wake up.",
-	"You were dreaming of someone and you heard their voice, you started to wake up.",
-	"You hear a small whisper in your ear, after that you hear loud footsteps fading away, you wake up being scared.",
-	"You hear a loud helicopter overhead of yourself and you instantly wake up.",
-	"You heard a growling and you started to wake up but scared.",
-	"You heard a loud siren and you are scared of the noise.",
-}
-local wakeupMessagesKor = {
+	-- "wakes up from a long nap and recover from your sleep.",
+	-- "stands up and sniffing the fresh air from the world.",
+	-- "starts to stand up and recover from your sleep.",
+	-- "are sweating from all the scare you have been getting from your dream and you wake up.",
+	-- "were dreaming of someone and you heard their voice, you started to wake up.",
+	-- "hears a small whisper in your ear, after that you hear loud footsteps fading away, you wake up being scared.",
+	-- "hears a loud helicopter overhead of yourself and you instantly wake up.",
+	-- "heard a growling and you started to wake up but scared.",
+	-- "heard a loud siren and you are scared of the noise.",
 	"긴 낮잠에서 깨어나 몸을 일으킵니다.",
 	"서늘한 바람을 느끼며 정신을 차립니다.",
 	"자리에서 일어나며 잠에서 깨어납니다.",
@@ -34,7 +32,7 @@ local wakeupMessagesKor = {
 	"멍하니 있다 어떤 익숙한 목소리를 듣고 정신을 차렸지만 그 사람의 모습은 온데간데 찾아볼 수 없었습니다.",
 	"귓가에 작은 속삭임이 들리다가 갑자기 누군가 달아나는 듯한 큰 발소리에 정신을 차립니다.",
 	"머리 위를 지나는 시끄러운 헬리콥터 소리에 곧장 정신이 듭니다.",
-	"좀비가 내는 그르륵 소리에 깜짝 놀라 정신을 차립니다.",
+	"그르륵 거리는 꺼림칙한 소리에 화들짝 놀라 정신을 차립니다.",
 	"시끄러운 사이렌 소리에 놀라 정신이 듭니다.",
 }
 
@@ -43,8 +41,8 @@ function PLUGIN:PlayerSpawn(ply)
 
 	ply:ConCommand("play music/stingers/hl1_stinger_song16.mp3")
 	ply:ScreenFade(SCREENFADE.IN, color_black, 3, 2)
+	
+	local msg = table.Random(wakeupMessages)
 
-	local msg = table.Random(wakeupMessagesKor)
-
-	-- ply:ChatPrint("─ "..msg)
+	ply:PlayerSay("/me "..msg)
 end
