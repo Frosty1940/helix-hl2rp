60b35edaaf14c1d3ef451605e6a705b224b2d34d
Frosty <thks1018@gmail.com>
2025-07-24 16:30:25 +0900

    yee

---
+++ plugins/languagetranslator.lua
@@ -0,0 +1,89 @@
+PLUGIN.name = "Language Translator"
+PLUGIN.author = "Shooter#5269"
+PLUGIN.description = "Translates most Text chat into the reservers language (Allowing for players to cross the language barrior)."
+
+ix.lang.AddTable("english", {
+	
+	optLanguageTranslator = "Language Translator",
+	optdLanguageTranslator = "The language you type in and want to reseave.",
+})
+ix.lang.AddTable("russian", {
+	optLanguageTranslator = "Переводчик языков",
+	optdLanguageTranslator = "Язык, который вы вводите и хотите получить.",
+})
+
+ix.option.Add("languageTranslator", ix.type.array, ix.config.languageTranslator or "en      ", {
+	category = "Language Translator",
+	bNetworked = true,
+	populate = function()
+		return {
+			["en      "] = "영어 (English)",
+			["ru      "] = "러시아어 (русский)",
+			["es      "] = "스페인어 (español)",
+			["fr      "] = "프랑스어 (français)",
+			["pl      "] = "폴란드어 (Polski)",
+			["uk      "] = "우크라이나어 (український)",
+			["zh-TW      "] = "중국어 (간체자) (简体中文)",
+			["zh-CN      "] = "중국어 (정자) (中國傳統的)",
+			["hi      "] = "힌디어 (हिन्दी)",
+			["ko      "] = "한국어 (Korean)"
+		}
+	end
+})
+
+-- https://wiki.facepunch.com/gmod/coroutine.wrap
+local function HTTPRequest( url, headers )
+	local running = coroutine.running()
+
+	local function onSuccess( body, length, header, code )
+		coroutine.resume( running, true, body, header, code )
+	end
+
+	local function onFailure( err )
+		coroutine.resume( running, false, err )
+	end
+
+	http.Fetch( url, onSuccess, onFailure, headers )
+
+	return coroutine.yield()
+end
+
+-- https://github.com/lunarmodules/luasocket/blob/master/src/url.lua
+local function escape(s) return (string.gsub(s, "([^A-Za-z0-9_])", function(c) return string.format("%%%02x", string.byte(c)) end)) end
+
+
+
+--Thank you, https://gist.github.com/tomill/362661 for insperation
+local function LanguageTranslator(speaker, receiver, query, chatType, rawText)
+	local WritenLang  = string.Replace(ix.option.Get(speaker, "languageTranslator", "en      "), " ", "")
+	local ReseaveLang = string.Replace(ix.option.Get(receiver, "languageTranslator", GetConVar("gmod_language")), " ", "")
+	local query = query or ""
+	if WritenLang == ReseaveLang or query == "" then return end
+	coroutine.wrap( function() 
+		local HTTPurl = "http://translate.googleapis.com/translate_a/single?client=gtx&sl="
+		..WritenLang 
+		.."&tl="
+		..ReseaveLang 
+		.."&dt=t&q="
+		..escape(query)
+
+		local state, response, header, code = HTTPRequest(HTTPurl)
+		if state then
+			local fullyTranslated = (util.JSONToTable(response))[1][1][1]
+			if fullyTranslated != query then
+				ix.chat.Send(speaker, chatType, fullyTranslated, false, {receiver})
+			end
+		else
+			print("(Contact Server Administation or Shooter#5269), code:".. code)
+		end
+	end)()
+end
+
+function PLUGIN:PlayerMessageSend(speaker, chatType, text, anonymous, receivers, rawText) 
+	if (IsValid(speaker) and receivers) then
+		for k, v in pairs(receivers) do
+			LanguageTranslator(speaker, v , text, chatType, rawText)
+		end
+	end
+end
+
